5d1e11a89672fbc265ca0659fd5af53a3173505b
Yoonoh <xkakak142@naver.com>
2022-03-15 23:34:30 +0900

    setting from spring initializer

---
+++ src/main/java/com/example/devstacknoticeservice/DevstackNoticeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.devstacknoticeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevstackNoticeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevstackNoticeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/devstacknoticeservice/DevstackNoticeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devstacknoticeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevstackNoticeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
